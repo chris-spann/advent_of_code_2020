4d4ae218b05872bd3afe9127a85f0d90557d7587
Chris Spann <cspann@lyft.com>
2020-12-03 11:39:18 -0600

    use math to get final sum instead

--- aoc_2020/day3.py
+++ aoc_2020/day3.py
@@ -1,3 +1,6 @@
+import math
+
+
 def get_data(filepath):
     file = open(filepath, 'r')
     lines = file.read().splitlines()
@@ -26,10 +29,7 @@ def get_product():
     res.append(traverse('aoc_2020/data/day3_data.txt', 5, 1))
     res.append(traverse('aoc_2020/data/day3_data.txt', 7, 1))
     res.append(traverse('aoc_2020/data/day3_data.txt', 1, 2))
-    result = 1
-    for x in res:
-        result = result * x
-    return result
+    return math.prod(res)
 
 
 print(get_product())
